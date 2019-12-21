f0722766028c11069ab6ff77bfef75751a7d59fd
Vladimir Serov <me@cab404.ru>
2019-12-21 14:48:34 +0300

    [fix] image reading now works

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, request
+# from check import get_catboost_pred 
 from PIL import Image
+import json
+import io
 
 app = Flask("mahalovo")
+
+def categorize(image):
+    return 1
 
 @app.route('/')
 def index():
@@ -9,8 +15,8 @@ def index():
 
 @app.route("/api/recognize", methods=["POST"])
 def recognize():
-    imgdata = request.get_data()
-    image = Image.frombytes("RGB", (320, 240), imgdata)
-    return "test"
+    raw_image = request.files["image"].read()
+    image = Image.open(io.BytesIO(raw_image))
+    return json.dumps({"category": categorize(image)})
 
 app.run(port=8080)
