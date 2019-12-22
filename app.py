e954450f4ca822396795e7351641e7471582db95
Vladimir Serov <me@cab404.ru>
2019-12-22 19:23:37 +0300

    [feat] rf is new default model

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def randomforest_categorize(image):
     log.info(f"[Random Forest] Predicted {pred} with P={acc} !")
     return (pred, acc)
 
-categorize = catboost_categorize
+categorize = randomforest_categorize
 
 @app.route('/')
 def index():
